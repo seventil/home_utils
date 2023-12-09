c826bc0613b02f54d4e49dad87762d2f74e57080
aleskutepov <ales.kutepov@gmail.com>
2023-12-09 15:26:55 +0300

    Added all nations to civ 6 drafter with option to exclude recent plays

--- civ6_drafter/draft.py
+++ civ6_drafter/draft.py
@@ -1,34 +1,62 @@
+from collections import namedtuple
+import json
 import random
 
-LEADER_PASS = [
-    "Abraham Lincoln - American",
-    "Nzinga Mbande - Kongolese",
-    "Tokugawa - Japanese",
-    "Nader Shah - Persian",
-    "Wu Zetian - Chinese",
-    "Yongle - Chinese",
-    "Ramses II - Egyptian",
-    "Sundiata Keita - Malian",
-    "Theodora - Byzantine",
-    "Sejong - Korean",
-    "Ludwig II - German",
-    "Elizabeth I - English",
-    "Saladin (Sultan) - Arabian",
-    "Suleiman (Muhteşem) - Ottoman",
-    "Qin Shi Huang (Unifier) - Chinese",
-    "Cleopatra (Ptolemaic) - Egyptian",
-    "Harald Hardrada (Varangian) - Norwegian",
-    "Victoria (Age of Steam) - English"
-]
+FILE_ENCODING = "utf-8"
+LEADERS_PATH = "leaders.json"
+PLAYERS_PATH = "player_history.json"
+
+
+DraftOutcome = namedtuple('DraftOutcome', 'name leader civ')
+
+
+class Leaders:
+    def __init__(self, path: str = LEADERS_PATH) -> None:
+        with open(path, encoding=FILE_ENCODING) as stream:
+            self.leads = json.load(stream)
+
+    def pick_random(self, player_recent: list) -> dict:
+        new_nations_for_player = [
+            nation 
+            for nation in self.leads 
+            if nation["leader name"] not in player_recent
+        ]
+        choice = random.choice(new_nations_for_player)
+        leftover_nations = [
+            nation
+            for nation in self.leads
+            if choice["civ"] != nation["civ"]
+        ]
+        self.leads = leftover_nations
+        return choice
+
+
+class Draft:
+    def __init__(self, todays_players: list, path: str = PLAYERS_PATH) -> None:
+        with open(path, encoding=FILE_ENCODING) as stream:
+            players = json.load(stream)
+            self.players = []
+            for player in players:
+                if player["name"] in todays_players:
+                    self.players.append(player)
+
+    def draft(self, leaders: Leaders) -> list:
+        output = []
+        for _ in range(len(self.players)):
+            player = random.choice(self.players)
+            self.players.remove(player)
+            nation = leaders.pick_random(player["played recently"])
+            output.append(
+                DraftOutcome(player["name"], nation["leader name"], nation["civ"])
+            )
+        return output
 
 
 def main():
-    players = ["Алесь", "Мишаня", "Саман", "Елизавета"]
-    leaders = LEADER_PASS[:]
-    for legend in players:
-        nation = random.choice(leaders)
-        leaders.remove(nation)
-        print(f"{legend} will play {nation}")
+    players = Draft(["Алесь", "Мишаня", "Саман", "Елизавета", "Вишня"])
+    leaders = Leaders()
+    for result in players.draft(leaders):
+        print(f"{result.name} играет за {result.leader}, {result.civ}")
 
 
 if __name__ == "__main__":
